f5489ace7deffda4f9e4038520f92dd83dba98ac
strub17u <strub17u@etu.univ-lorraine.fr>
2022-02-11 12:08:13 +0100

    Commencement VueLettres fin de séance...

--- src/boggle/model/vue/VueLettres.java
+++ src/boggle/model/vue/VueLettres.java
@@ -1,10 +1,19 @@
 package boggle.model.vue;
 import boggle.model.Boggle;
 import boggle.model.Observateur;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.TilePane;
 
 public class VueLettres extends TilePane implements Observateur{
+    private Button[] bou;
+
+    public VueLettres(Boggle bog){
+        int nbBouton = bog.getTaille();
+        for(int i = 0; i < nbBouton; i++){
+
+        }
+    }
 
     @Override
     public void reagir() {
